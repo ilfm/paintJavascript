191845b816f5ffbf0e5c461c664e2328f64970f6
ilfm <dnehd1828@gmail.com>
2021-01-21 18:11:47 +0900

    21.01.21 app.js

--- WebContent/app.js
+++ WebContent/app.js
@@ -4,3 +4,13 @@
 
 const canvas = document.getElementById("jsCanvas");
 
+function onMousemove(event)
+{
+	console.log(event);
+}
+
+
+if(canvas)
+{
+	canvas.addEventListener("mousemove",onMousemove);
+}
